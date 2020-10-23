ee0cdad682f04943490c9fb7b4320fe520dbb1fa
GmoneyAlan <alan200@outlook.com>
2020-10-23 05:45:37 +0000

    server.py added

---
+++ server.py
@@ -0,0 +1,44 @@
+import socket
+import threading
+
+'''
+Constant values that 'should' not change at all
+    includes the server, port, format, and disconnect msg
+'''
+PORT = 5049
+SERVER = '192.168.0.7' 
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!DISCONNECT'
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(connection, addr):
+    
+    print(f'[NEW CONNECTION] {addr} connected')
+    connected = True
+    while connected:
+        msg_len = connection.recv(64).decode(FORMAT)
+        if msg_len:
+            msg_len = int(msg_len)
+            msg = connection.recv(msg_len).decode(FORMAT)
+
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f'[{addr}] {msg}')
+            connection.send('Message received'.encode(FORMAT))
+
+def start():
+    server.listen()
+    print(f'Server is listening on {SERVER} PORT:{PORT}')
+    while True:
+        connection, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(connection, addr))
+        thread.start()
+        #print(f'[ACTIVE CONNECTIONS] {thread.active_count() - 1}')
+
+print('Server is starting...')
+start()
+
